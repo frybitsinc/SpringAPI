3ade434265cf42f7185db081e6dcd0ca6fa0b877
DESKTOP-3987IAG\rk <arkang@smallticket.com>
2020-07-10 18:27:42 +0900

    first commit

---
+++ src/main/java/com/frybitsinc/springdemo1.java
@@ -0,0 +1,14 @@
+package com.frybitsinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springdemo1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springdemo1.class, args);
+	}
+
+}
