9056655c4ac847f78fc72548d2a032de52193b2a
DESKTOP-3987IAG\rk <arkang@smallticket.com>
2020-07-14 10:53:06 +0900

    simple get api

---
+++ src/main/java/com/frybitsinc/controller/HelloController.java
@@ -0,0 +1,61 @@
+package com.frybitsinc.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	// URL Mapping
+	@RequestMapping("/")
+	public String home() {
+		return "Home";
+	}
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi~";
+	}
+	
+	@RequestMapping("/get/member")
+	public Map<String, Object> getMemberList() throws Exception{
+		List<Map<String, Object>> memList = new ArrayList<>();
+		Map<String, Object> memInfo = new HashMap<>();
+		
+		memInfo.put("id", "1");
+		memInfo.put("name", "스텔라");
+		memInfo.put("imageURL", "https://www.smallticket.com/file/down/eG-DXSagqWqEYNmt44nsD_8oMYzy-UMIicMcIiQydVI");
+		memInfo.put("isFamilyProfile", true); // 가족프로필 여부
+		memInfo.put("isAddInfo", false); // 추가정보 입력필요 여부
+		memList.add(memInfo);
+		
+		return genMap(200, "msg", "data", memList);
+	}
+	
+	@RequestMapping("/get/member/{member_id}")
+	public Map<String, Object> getMember(@RequestParam(name = "member_id", required = false) String memberId) throws Exception{
+		return genMap(200, memberId);
+	}
+	
+	// Mapping 함수
+	public HashMap<String, Object> genMap(int status, String message) {
+		HashMap<String, Object> ret = new HashMap<>();
+		ret.put("status", status);
+		ret.put("message", message);
+		return ret;
+	}
+	
+	public HashMap<String, Object> genMap(int status, String message, String key, Object value) {
+		HashMap<String, Object> ret = new HashMap<>();
+		ret.put("status", status);
+		ret.put("message", message);
+		ret.put(key, value);
+		return ret;
+	}
+}
