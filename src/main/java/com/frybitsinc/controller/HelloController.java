cfae8030d07f5c89107932623ff593f0e6bb97dd
DESKTOP-3987IAG\rk <arkang@smallticket.com>
2020-07-15 18:10:24 +0900

    final final

--- src/main/java/com/frybitsinc/controller/HelloController.java
+++ src/main/java/com/frybitsinc/controller/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.frybitsinc.thymeleaf.model.Car;
-import org.springframework.validation.BindingResult;
 
 @Controller
 @RequestMapping("/")
