ac3c9b404ec961ff43b00c6c1d49ea94c95a8015
DESKTOP-3987IAG\rk <arkang@smallticket.com>
2020-07-15 17:21:37 +0900

    whyy..

--- src/main/java/com/frybitsinc/controller/HelloController.java
+++ src/main/java/com/frybitsinc/controller/HelloController.java
@@ -1,35 +1,26 @@
 package com.frybitsinc.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.frybitsinc.thymeleaf.model.Car;
+import com.frybitsinc.thymeleaf.model.CarModel;
+import com.frybitsinc.thymeleaf.model.MenuModel;
 
 @Controller
 @RequestMapping({ "/", "/index" })
 public class HelloController {
 
     @GetMapping
-    public String main(Model model) {
+    public String index(Model model) {
         model.addAttribute("car", new Car());
         return "index";
     }
 
     @PostMapping
-    public String save(Car car, Model model) {
+    public String saved(Car car, Model model) {
         model.addAttribute("car", car);
         return "saved";
     }
